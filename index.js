d1218b69ad91d48b6e6dec2c404c655976af4d41
emonmahdi <developeremon1997@gmail.com>
2021-10-30 10:49:56 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config()
+const app = express();
+const port = process.env.PORT || 5000
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tlqgi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+console.log(uri)
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+async function run (){
+    try{
+        await client.connect(); 
+        const database = client.db('hotelServices');
+        const serviceCollection = database.collection('roomService');
+
+        // GET Service API
+        app.get('/rooms', async(req,res) => {
+            const cursor = serviceCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+
+    }finally{
+        // await client.close()
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hotel Grand Park is Running')
+})
+
+app.listen(port, () => {
+  console.log('Hotel Grand Park Server is running port', port)
+})
